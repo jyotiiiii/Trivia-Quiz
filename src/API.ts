f2a69590cf2f9a99547ec0a7313c6d65ae584464
Jyoti <jyoti.contact@gmail.com>
2020-07-17 19:22:42 +0100

    add api fetch

---
+++ src/API.ts
@@ -0,0 +1,42 @@
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+
+}
+//combine correct_answer and incorrect_answers into one array to map in UI
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export enum Category {
+  ANIMALS = 27,
+  ART = 25,
+  CELEBRITIES = 26,
+  FILM = 11,
+  GENERAL = 9,
+  HISTORY = 23,
+  TELEVISION = 14,
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty,
+  category: Category
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answer:
+      }
+  ))
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@
 // font - family: 'Fascinate Inline', cursive;
 
 import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
-
+// Types
+import { Difficulty, Category } from './API';
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
@@ -15,6 +17,10 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameover, setGameover] = useState(true);
 
+  console.log(
+    fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY, Category.CELEBRITIES)
+  );
+
   const startQuiz = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -24,17 +30,19 @@ const App = () => {
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startQuiz}></button>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
       <p className="score">Score:</p>
       <p className="">Loading Questions...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
