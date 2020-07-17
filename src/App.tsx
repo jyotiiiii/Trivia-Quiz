a12d13cdfac2847b505fddce2b33721e16494c0e
Jyoti <jyoti.contact@gmail.com>
2020-07-17 16:07:56 +0100

    create QuestionCard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@
 // font - family: 'Fascinate Inline', cursive;
 
 import React from 'react';
+// Components
+import QuestionCard from './components/QuestionCard';
 
-function App() {
-  return <div className="App">Quiz</div>;
-}
+const App = () => {
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}></button>
+      <p className="score">Score:</p>
+      <p className="">Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
