49257044109fbb8ee0f4ad9e547a008df6ffe216
Jyoti <jyoti.contact@gmail.com>
2020-07-17 20:40:28 +0100

    implement checkAnswer function

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,7 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameover, setGameover] = useState(true);
 
-  console.log(
-    fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY, Category.CELEBRITIES)
-  );
+  console.log(questions);
   // put some error handling here try catch block
   const startQuiz = async () => {
     setLoading(true);
@@ -46,29 +44,61 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameover) {
+      // users answer
+      const answer = e.currentTarget.value;
+      // check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // add score if answer is correct
+      if (correct) {
+        setScore((prev) => prev + 1);
+        // save the answer in the array of user answers
+        const AnswerObject = {
+          question: questions[number].question,
+          answer,
+          correct,
+          correctAnswer: questions[number].correct_answer,
+        };
+        setUserAnswers((prev) => [...prev, AnswerObject]);
+      }
+    }
+  };
 
   const nextQuestion = () => {};
 
   return (
     <div className="App">
       <h1>React Quiz</h1>
-      <button className="start" onClick={startQuiz}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p className="">Loading Questions...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {gameover || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startQuiz}>
+          Start
+        </button>
+      ) : null}
+
+      {!gameover || userAnswers.length === TOTAL_QUESTIONS ? (
+        <p className="score">Score:</p>
+      ) : null}
+      {/* && is short circuit for ternary where false returns null */}
+      {loading && <p className="">Loading Questions...</p>}
+      {!loading && !gameover && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!loading &&
+      !gameover &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };
